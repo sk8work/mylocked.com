6c8a1d604751ac6ff9ed7783981135fc1984c780
sk8work <eshikhantsev@gmail.com>
2021-10-17 18:15:57 +0300

    Created our first web server!

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
 
 func main()  {
-	fmt.Println("Hello world!!")
+	http.HandleFunc("/", handlerFunc);
+	fmt.Println("Listening on port 3000")
+	http.ListenAndServe(":3000", nil)
+
 }
